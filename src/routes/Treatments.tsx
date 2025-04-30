cbf3fb6dd377b542494dfe120e9f6a139e7b8de4
skyrix-lang <55582341+skyrix-lang@users.noreply.github.com>
2025-04-30 03:22:55 +0200

    Update Treatments.tsx

--- src/routes/Treatments.tsx
+++ src/routes/Treatments.tsx
@@ -30,7 +30,7 @@ const Treatments: FC = () => {
     <>
       <SEOMetadata
         title="Relaxa'Son - Les soins proposés"
-        description="Explorez la puissance thérapeutique des sons et vibrations. Nos sessions de sonothérapie vous aident à réduire le stress, améliorer le sommeil et retrouver l'équilibre intérieur."
+        description="Une approche holistique qui utilise les vibrations et les fréquences sonores pour apaiser le mental, favoriser l'équilibre intérieur, la relaxation et le bien-être."
         image={massage}
         url="https://relaxason.com/soins"
         type="article"
